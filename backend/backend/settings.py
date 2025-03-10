57650be3196e01b8615a3417751c15905e4fc975
Emircan Çelik <emircancelik0102@gmail.com>
2025-03-11 02:32:12 +0300

    database ayarları

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kiel_db',  # Veritabanı adı
+        'USER': 'postgres',  # PostgreSQL kullanıcı adı
+        'PASSWORD': 'sifre',  # PostgreSQL şifren
+        'HOST': 'localhost',  # Sunucu adresi (cloud kullanıyorsan değişebilir)
+        'PORT': '5432',  # PostgreSQL varsayılan portu
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
